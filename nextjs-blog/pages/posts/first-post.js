8b576d174d29d4bf34b7ada1343edc157bcc919e
Dunitt Monagas <dunitt@example.com>
2022-05-12 00:41:41 -0400

    Assets, Metadata, and CSS - Layout Component

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,25 +1,15 @@
 
 
-import Link from 'next/link';
 import Head from 'next/head';
-import Script from 'next/script';
-
-
+import Link from 'next/link';
 import Layout from '../../components/layout';
 
 
 export default function FirstPost() {
   return (
     <Layout>
-    	<Head>
+      <Head>
         <title>First Post</title>
-        <Script
-	        src="https://connect.facebook.net/en_US/sdk.js"
-	        strategy="lazyOnload"
-	        onLoad={() =>
-	          console.log(`script loaded correctly, window.FB has been populated`)
-	        }
-	      />
       </Head>
       <h1>First Post</h1>
       <h2>
